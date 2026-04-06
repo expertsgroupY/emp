49462a41b1e4d1eafaeb6ef28375a396a7c7388e
expertsgroupY <supexpmail@gmail.com>
2026-04-06 14:12:24 +0200

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Experts Group | ERP Systems Egypt",
+  description: "Experts Group for Financial & Management Systems"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
